9bbfde5e8220bbb76081483d2056b6a0401dc1aa
villalb4 <villalb4angel@gmail.com>
2023-01-24 17:25:53 -0300

    change: background and styles

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Search.css'
-import searchBg from '../../Assets/backgroundImg.png'
+import searchBg from '../../Assets/backgroundSearch.jpg'
+import work_icon from '../../Assets/icons/work.svg'
 
 function Search() {
   return (
@@ -8,7 +9,7 @@ function Search() {
       <div className='Search_content' style={{backgroundImage: `url(${searchBg})`}}>
         <form className='Search_searcher'>
           <div className='Search_divInput'>
-            <img src="" alt="" />
+            <img src={work_icon} alt="" />
             <input type="text" placeholder='Title, companies, expertise or benefits' />
           </div>
           <div className='Search_divButton'>
