b68c78906f6553b23bc0a90ca84752760c53de48
villalb4 <villalb4angel@gmail.com>
2023-01-24 05:07:58 -0300

    add: created Page and import search Component

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Search from '../../Components/Search/Search'
+
+function Home() {
+  return (
+    <div>
+      <Search />
+    </div>
+  )
+}
+
+export default Home
