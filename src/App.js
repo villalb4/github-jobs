12f430aa008c2860068a6d9dc6a01eeb899007b2
villalb4 <villalb4angel@gmail.com>
2023-01-24 05:06:22 -0300

    add: import Header and Home

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Header from './Components/Header/Header';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Home />
     </div>
   );
 }
