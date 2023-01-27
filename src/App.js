ade95484ed4ceda6e31ec2ec288ce4e5d544c003
villalb4 <villalb4angel@gmail.com>
2023-01-27 13:29:53 -0300

    add: dispatch getJobs thunk

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { useEffect } from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
 import Home from './Pages/Home/Home';
+import { useDispatch } from 'react-redux';
+import { getJobs } from './Redux/thunk/getJobs';
 
 function App() {
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getJobs())
+  }, [])
+
   return (
     <div className="App">
       <Header />
