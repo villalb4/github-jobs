7129bcc331bd87b4ed857ff7a3f47344ab70cf56
villalb4 <villalb4angel@gmail.com>
2023-01-28 19:48:24 -0300

    add: jobsApi RTK query

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { jobsApi } from "./apis/jobs";
 import jobs from "./slice/jobs"
 
 export const store = configureStore({
   reducer: {
     jobs,
-  }
+
+    [jobsApi.reducerPath]: jobsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+    .concat(jobsApi.middleware)
 })
