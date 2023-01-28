7403f5cc31ddf22aac9bd14c95ae1a212792fc3e
villalb4 <villalb4angel@gmail.com>
2023-01-28 03:45:54 -0300

    add: creating and styling component

---
+++ src/Components/Filters/Filters.jsx
@@ -0,0 +1,45 @@
+import './Filters.css'
+import planet from '../../Assets/icons/planet.svg'
+
+function Filters() {
+  return (
+    <div className='Filters'>
+      <form>
+        {/* --- Full time filter --- */}
+        <div className='Filters_divFullTime'>
+          <input type="checkbox" name="full_time"/>
+          <label htmlFor="full_time">Full Time</label>
+        </div>
+        {/* --- Location filter --- */}
+        <div className='Filters_divLocation'>
+          <h2>LOCATION</h2>
+          <div>
+            <img src={planet} alt="" />
+            <input type="text" placeholder='City, state, zip, code our country'/>
+          </div>
+        </div>
+        {/* --- Citys filter --- */}
+        <div className='Filters_divCitys'>
+          <div className='Filters_divCityInput'>
+            <input type="radio" name="city1"/>
+            <label htmlFor="city1">London</label>
+          </div>
+          <div className='Filters_divCityInput'>
+            <input type="radio" name="city2"/>
+            <label htmlFor="city2">Amsterdam</label>
+          </div>
+          <div className='Filters_divCityInput'>
+            <input type="radio" name="city3"/>
+            <label htmlFor="city3">New York</label>
+          </div>
+          <div className='Filters_divCityInput'>
+            <input type="radio" name="city4"/>
+            <label htmlFor="city4">Berlin</label>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Filters
