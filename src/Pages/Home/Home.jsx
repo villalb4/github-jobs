098768d3569aa5ba1f48d53b5d1efa415d6b2fbd
villalb4 <villalb4angel@gmail.com>
2023-01-28 03:46:15 -0300

    add: inport Filters component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Filters from '../../Components/Filters/Filters'
 import Search from '../../Components/Search/Search'
 
 function Home() {
   return (
     <div>
       <Search />
+      <section>
+        <Filters />
+      </section>
     </div>
   )
 }
