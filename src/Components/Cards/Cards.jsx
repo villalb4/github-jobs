fd8555bfd1d987ce699d550812c0dc79857bad4d
villalb4 <villalb4angel@gmail.com>
2023-01-29 04:03:40 -0300

    add: adapters and styles

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-import { useGetJobsQuery } from '../../Store/apis/jobs'
+import { useGetJobsQuery } from '../../Store/api/jobs'
 import './Cards.css'
 import planet from '../../Assets/icons/planet.svg'
 import clock from '../../Assets/icons/clock.svg'
+import dateAdapter from '../../Adapters/dateAdapter'
+import locationAdapter from '../../Adapters/locationAdapter'
+import jobTypeAdapter from '../../Adapters/jobTypeAdapter'
 
 function Cards() {
 
@@ -26,18 +29,18 @@ function Cards() {
                     <span className='Cards_cardJobTitle'>{e.title}</span>
                     <div>
                       <div className='Cards_cardDivJobType'>
-                        <span>{e.job_type}</span>
+                        <span>{jobTypeAdapter(e.job_type)}</span>
                       </div>
                     </div>
                   </div>
                   <div className='Cards_cardDateLocation'>
                     <div className='Cards_cardDivLocation'>
                       <img src={planet} alt="" />
-                      <span>{e.candidate_required_location}</span>
+                      <span>{locationAdapter(e.candidate_required_location)}</span>
                     </div>
                     <div className='Cards_cardDivDate'>
                       <img src={clock} alt="" />
-                      <span>{e.publication_date}</span>
+                      <span>{dateAdapter(e.publication_date)}</span>
                     </div>
                   </div>
                 </div>
