c6bbac2251c20d82d871e4b0567a359899ca980b
villalb4 <villalb4angel@gmail.com>
2023-01-29 20:14:55 -0300

    change: delete rtk query

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,13 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { jobsApi } from "./api/jobs";
 import jobs from "./slice/jobs"
 
 export const store = configureStore({
   reducer: {
     jobs,
-
-    [jobsApi.reducerPath]: jobsApi.reducer,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware()
-    .concat(jobsApi.middleware)
+  }
 })
