f35702e3ab7cb2508261ca853b1df155a66db4c9
villalb4 <villalb4angel@gmail.com>
2023-01-29 20:13:14 -0300

    add: create thunk

---
+++ src/Store/thunks/jobsFetchThunk.js
@@ -0,0 +1,14 @@
+import { setJobs, startLoadingJobs } from "../slice/jobs"
+import jobsAdapter from '../../Adapters/jobsAdapter'
+
+export const getJobs = () => {
+  return async( dispatch, getState ) => {
+    dispatch(startLoadingJobs())
+    const response = await fetch('https://remotive.com/api/remote-jobs?category=software-dev&limit=5')
+    const data = await response.json()
+
+    const dataFiltered = jobsAdapter(data)
+   
+    dispatch(setJobs(dataFiltered))
+  }
+}
