15ad322df8e9d4e0b89a4f8e8b169066d8630f7c
villalb4 <villalb4angel@gmail.com>
2023-01-29 04:01:46 -0300

    add: change import useGetJobsQuery

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import Header from './Components/Header/Header';
 import Skeleton from './Components/Skeleton/Skeleton';
 import Home from './Pages/Home/Home';
-import { useGetJobsQuery } from './Store/apis/jobs';
+import { useGetJobsQuery } from './Store/api/jobs';
 
 function App() {
 
   const { data, isLoading } = useGetJobsQuery()
-  console.log(data)
+  // console.log(data)
 
   return (
     <div className="App">
