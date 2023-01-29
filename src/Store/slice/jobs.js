e6251a05d6197cb5655c691bda8d93fd98323ff4
villalb4 <villalb4angel@gmail.com>
2023-01-29 20:12:46 -0300

    add: set jobs and isLoading

--- src/Store/slice/jobs.js
+++ src/Store/slice/jobs.js
@@ -1,18 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  allJobs : []
+  allJobs : [],
+  isLoading: true,
 }
 
 const jobsSlice = createSlice({
   name: "jobs",
   initialState,
   reducers: {
+    startLoadingJobs: (state, action) => {
+      state.isLoading = true;
+    },
     setJobs: (state, action) => {
-      state.allJobs = action.payload;
+      state.allJobs = action.payload
+      state.isLoading = false;
     }
   }
 })
 
-export const { setJobs } = jobsSlice.actions
+export const { setJobs, startLoadingJobs } = jobsSlice.actions
 export default jobsSlice.reducer
