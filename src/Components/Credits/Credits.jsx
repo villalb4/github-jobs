ebb4e08653f140451c0ad556c68e5a81a561976d
villalb4 <villalb4angel@gmail.com>
2023-01-30 20:59:47 -0300

    add: creating and styling component

---
+++ src/Components/Credits/Credits.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Credits.css'
+
+function Credits() {
+  return (
+    <div className='Credits'>
+      <span>created by <a href="https://github.com/villalb4">villalb4</a> - devChallenges.io</span>
+    </div>
+  )
+}
+
+export default Credits
