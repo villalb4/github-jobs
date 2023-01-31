110dc60670d7ea6e576ef94f53686a70a26316d2
villalb4 <villalb4angel@gmail.com>
2023-01-30 21:03:09 -0300

    change: api limit

--- src/Store/thunks/jobsFetchThunk.js
+++ src/Store/thunks/jobsFetchThunk.js
@@ -4,7 +4,7 @@ import jobsAdapter from '../../Adapters/jobsAdapter'
 export const getJobs = () => {
   return async( dispatch, getState ) => {
     dispatch(startLoadingJobs())
-    const response = await fetch('https://remotive.com/api/remote-jobs?category=software-dev&limit=5')
+    const response = await fetch('https://remotive.com/api/remote-jobs?category=software-dev&limit=100')
     const data = await response.json()
 
     const dataFiltered = jobsAdapter(data)
