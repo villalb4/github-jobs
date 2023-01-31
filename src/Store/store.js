339e76b8540c1280bafd170db94f50d26c39f71c
villalb4 <villalb4angel@gmail.com>
2023-01-30 21:01:16 -0300

    add: pagination slice

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import jobs from "./slice/jobs"
+import pagination from "./slice/pagination";
 
 export const store = configureStore({
   reducer: {
     jobs,
+    pagination
   }
 })
