cc3c704d5c00a9ac1ad2277901e79db8e2872a3c
villalb4 <villalb4angel@gmail.com>
2023-02-03 20:45:38 -0300

    add: creating and structuring component

---
+++ src/Components/Apply/Apply.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Apply.css'
+
+function Apply() {
+  return (
+    <div className='Apply'>
+      <h3 className='Apply_h3'>HOW TO APPLY</h3>
+      <p className='Apply_HowTo'>Please email a copy of your resume and online portfolio to</p>
+      <p className='Apply_emails'><span>example@email.com</span> & CC <span>recruiter@email.com</span></p>
+    </div>
+  )
+}
+
+export default Apply
