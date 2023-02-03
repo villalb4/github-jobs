2b13d195fbb8f31d180b7870f00392510ba9a3f0
villalb4 <villalb4angel@gmail.com>
2023-02-03 17:08:21 -0300

    add: Header nad homeSkeleton components

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,10 +5,19 @@ import Filters from '../../Components/Filters/Filters'
 import Search from '../../Components/Search/Search'
 import Pagination from '../../Components/Pagination/Pagination'
 import Credits from '../../Components/Credits/Credits'
+import Header from '../../Components/Header/Header'
+import useFetchJobs from '../../Hooks/useFetchJobs'
+import HomeSkeleton from '../../Components/Skeletons/HomeSkeleton/HomeSkeleton'
 
 function Home() {
+
+  const { isLoading } = useFetchJobs()
+
+  if(isLoading) return <HomeSkeleton />
+
   return (
     <div className='Home'>
+      <Header />
       <Search />
       <div className='Home_divMain'>
         <Filters />
