7db6eecbad9df21cd6583a7514ebca88b8e36913
villalb4 <villalb4angel@gmail.com>
2023-02-03 08:30:31 -0300

    ningun cambio

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -12,36 +12,36 @@ function Cards() {
   return (
     <div className='Cards'>
       {
-          currentJobs.map((e, i) => {
-            return (
-              <Link to={`/job/${e.id}`} className='Cards_card' key={i}>
-                <div className='Cards_cardDivLogo'>
-                  <img src={e.company_logo} alt="company logo" />
-                </div>
-                <div className='Cards_cardDivInfo'>
-                  <div className='Cards_cardInfo'>
-                    <span className='Cards_cardCompanyName'>{e.company_name}</span>
-                    <span className='Cards_cardJobTitle'>{e.title}</span>
-                    <div>
-                      <div className='Cards_cardDivJobType'>
-                        <span>{e.job_type}</span>
-                      </div>
-                    </div>
-                  </div>
-                  <div className='Cards_cardDateLocation'>
-                    <div className='Cards_cardDivLocation'>
-                      <img src={planet} alt="" />
-                      <span>{e.location}</span>
-                    </div>
-                    <div className='Cards_cardDivDate'>
-                      <img src={clock} alt="" />
-                      <span>{e.date}</span>
+        currentJobs.map((e, i) => {
+          return (
+            <Link to={`/job/${e.id}`} className='Cards_card' key={i}>
+              <div className='Cards_cardDivLogo'>
+                <img src={e.company_logo} alt="company logo" />
+              </div>
+              <div className='Cards_cardDivInfo'>
+                <div className='Cards_cardInfo'>
+                  <span className='Cards_cardCompanyName'>{e.company_name}</span>
+                  <span className='Cards_cardJobTitle'>{e.title}</span>
+                  <div>
+                    <div className='Cards_cardDivJobType'>
+                      <span>{e.job_type}</span>
                     </div>
                   </div>
                 </div>
-              </Link>
-            )
-          })
+                <div className='Cards_cardDateLocation'>
+                  <div className='Cards_cardDivLocation'>
+                    <img src={planet} alt="" />
+                    <span>{e.location}</span>
+                  </div>
+                  <div className='Cards_cardDivDate'>
+                    <img src={clock} alt="" />
+                    <span>{e.date}</span>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          )
+        })
       }
     </div>
   )
