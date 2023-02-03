4d6959ae4c86c7fc5e698316b7a7ecec95493c72
villalb4 <villalb4angel@gmail.com>
2023-02-03 20:45:02 -0300

    add: jobDescriptionUtil

--- src/Adapters/jobsAdapter.js
+++ src/Adapters/jobsAdapter.js
@@ -1,6 +1,8 @@
 import jobTypeUtil from "../Utils/jobTypeUtil"
 import locationUtil from "../Utils/locationUtil"
 import dateUtil from "../Utils/dateUtil"
+import jobDescriptionUtil from "../Utils/jobDescriptionUtil"
+
 
 const jobsAdapter = (data) => {
   const adapting = data.jobs.map(e => {
@@ -9,7 +11,7 @@ const jobsAdapter = (data) => {
       location: locationUtil(e.candidate_required_location),
       company_logo: e.company_logo,
       company_name: e.company_name,
-      description: e.description,
+      description: jobDescriptionUtil(e.description),
       job_type: jobTypeUtil(e.job_type),
       title: e.title,
       date: dateUtil(e.publication_date),
