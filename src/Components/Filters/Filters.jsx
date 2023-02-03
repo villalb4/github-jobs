191f00467f5aa6c3deeb2e860899c2feb469b3a9
villalb4 <villalb4angel@gmail.com>
2023-02-03 08:29:49 -0300

    refactor: data & add filter functions

--- src/Components/Filters/Filters.jsx
+++ src/Components/Filters/Filters.jsx
@@ -1,43 +1,52 @@
+import { useState } from 'react'
 import './Filters.css'
 import planet from '../../Assets/icons/planet.svg'
 
 function Filters() {
+
+  const data = [
+    {location: "USA"},
+    {location: "UK"},
+    {location: "LATAM"},
+    {location: "Worldwide"},
+  ]
+
+  const [fullTime, setFulltime] = useState(false)
+
+  const handleFullTime = () => {
+    setFulltime(e => !e)
+    // console.log(fullTime)
+  }
+  
+  const handleLocation = (e) => {
+    // console.log(e.target.value)
+  }
+
   return (
     <div className='Filters'>
-      <form>
-        {/* --- Full time filter --- */}
-        <div className='Filters_divFullTime'>
-          <input type="checkbox" name="full_time"/>
-          <label htmlFor="full_time">Full time</label>
+      {/* --- Full time filter --- */}
+      <div className='Filters_divFullTime'>
+        <input type="checkbox" name="full_time" value={fullTime} onClick={handleFullTime}/>
+        <label htmlFor="full_time">Full time</label>
+      </div>
+      {/* --- Location filter --- */}
+      <div className='Filters_divLocation'>
+        <h2>LOCATION</h2>
+        <div>
+          <img src={planet} alt="" />
+          <input type="text" placeholder='City, state, zip, code our country'/>
         </div>
-        {/* --- Location filter --- */}
-        <div className='Filters_divLocation'>
-          <h2>LOCATION</h2>
-          <div>
-            <img src={planet} alt="" />
-            <input type="text" placeholder='City, state, zip, code our country'/>
-          </div>
-        </div>
-        {/* --- Citys filter --- */}
-        <div className='Filters_divCitys'>
-          <div className='Filters_divCityInput'>
-            <input type="radio" name="city1"/>
-            <label htmlFor="city1">London</label>
-          </div>
-          <div className='Filters_divCityInput'>
-            <input type="radio" name="city2"/>
-            <label htmlFor="city2">Amsterdam</label>
-          </div>
-          <div className='Filters_divCityInput'>
-            <input type="radio" name="city3"/>
-            <label htmlFor="city3">New York</label>
-          </div>
-          <div className='Filters_divCityInput'>
-            <input type="radio" name="city4"/>
-            <label htmlFor="city4">Berlin</label>
-          </div>
-        </div>
-      </form>
+      </div>
+      {/* --- Citys filter --- */}
+      <div className='Filters_divCitys'>
+        {data.map((e, i) => (
+            <div className='Filters_divCityInput' key={i}>
+              <input type="radio" name="location" value={e.location} onClick={handleLocation}/>
+              <label htmlFor="city1">{e.location}</label>
+            </div>
+          )
+        )}
+      </div>
     </div>
   )
 }
