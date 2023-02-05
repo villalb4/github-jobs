c920f8f0e6a6cf29bc31b0d85825742d6df48af3
villalb4 <villalb4angel@gmail.com>
2023-02-04 22:41:04 -0300

    add: structuring and styling component

--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -5,6 +5,9 @@ import Header from '../../Components/Header/Header'
 import Apply from '../../Components/Apply/Apply'
 import useFetchJobs from '../../Hooks/useFetchJobs'
 import './Details.css'
+import back_icon from '../../Assets/icons/back.svg'
+import planet_icon from '../../Assets/icons/planet.svg'
+import clock_icon from '../../Assets/icons/clock.svg'
 
 function Details() {
   const { jobId } = useParams()
@@ -24,23 +27,35 @@ function Details() {
       <div className='Details_applyDescription'>
         <div>
           <div className='Details_divBack'>
+            <img src={back_icon} alt="" />
             <span className='Details_backToSearch'>Back to search</span>
           </div>
           <Apply />
         </div>
         <section className='Details_section'>
-          <h2>{jobDetail.title}</h2>
-          <span>{jobDetail.date}</span>
-          <div>
-            <div>
-              <img src={jobDetail.company_logo} alt="logo" />
-            </div>
-            <div>
-              <span>{jobDetail.company_name}</span>
-              <span>{jobDetail.locaiton}</span>
+          <div className='Details_divTitleType'>
+            <h2 className='Deatils_jobTitle'>{jobDetail.title}</h2>
+            <div className='Details_divJobType'>
+              <span>{jobDetail.job_type}</span>
             </div>
           </div>
-          <div>
+          <div className='Details_divDate'>
+            <img src={clock_icon} alt="" />
+            <span>{jobDetail.date}</span>
+          </div>
+          <div className='Details_divCompanyInfo'>
+            <div className='Details_divImage'>
+              <img src={jobDetail.company_logo} alt={`${jobDetail.company_name} logo`} />
+            </div>
+            <div className='Details_divNameLocation'>
+              <span>{jobDetail.company_name}</span>
+              <div>
+                <img src={planet_icon} alt="" />
+                <span>{jobDetail.location}</span>
+              </div>
+            </div>
+          </div>
+          <div className='Details_divDescription'>
             <p>{jobDetail.description}</p>
           </div>
         </section>
